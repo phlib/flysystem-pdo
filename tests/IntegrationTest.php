0db03a0fe1adf87dcf121a66e872967dbf419bbe
Luke Richards <luke.richards@emailcenteruk.com>
2016-03-03 13:04:22 +0000

    Format code of character range

--- tests/IntegrationTest.php
+++ tests/IntegrationTest.php
@@ -42,7 +42,11 @@ class IntegrationTest extends \PHPUnit_Extensions_Database_TestCase
     /**
      * @var array
      */
-    protected $charRange = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','.',' '];
+    protected $charRange = [
+        0,1,2,3,4,5,6,7,8,9,
+        'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+        '.',' '
+    ];
 
     /**
      * @var string|false
