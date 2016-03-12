331c2204ee273c305a88f2af066ddf4ef9fdc97d
Luke Richards <luke.richards@emailcenteruk.com>
2016-03-12 12:23:05 +0000

    Correct the SQLite schema definition

--- schema/sqlite.sql
+++ schema/sqlite.sql
@@ -1,19 +1,19 @@
 
 CREATE TABLE flysystem_path (
-  path_id INTEGER PRIMARY KEY,
+  path_id INTEGER NOT NULL,
   type TEXT NOT NULL,
-  path TEXT NOT NULL UNIQUE,
-  type TEXT NOT NULL,
-  contents BLOB,
-  size INTEGER NOT NULL DEFAULT 0,
+  path TEXT NOT NULL,
   mimetype TEXT,
-  timestamp INTEGER NOT NULL DEFAULT 0,
+  visibility TEXT,
+  size INTEGER DEFAULT NULL,
+  is_compressed INTEGER DEFAULT 1,
+  update_ts TEXT NOT NULL DEFAULT 0,
   PRIMARY KEY (path_id)
 );
 
 CREATE TABLE flysystem_chunk (
-  path_id MEDIUMINT UNSIGNED NOT NULL,
-  chunk_no SMALLINT(5) UNSIGNED NOT NULL,
-  content longblob NOT NULL,
+  path_id INTEGER NOT NULL,
+  chunk_no INTEGER NOT NULL,
+  content BLOB NOT NULL,
   PRIMARY KEY (path_id, chunk_no)
 );
